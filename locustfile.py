d89a2c01a7b59c7b591f934f488deb4be52e2d09
Steve Jackson <steve@testdouble.com>
2018-01-06 12:19:22 -0500

    Signup start

--- locustfile.py
+++ locustfile.py
@@ -1,8 +1,21 @@
-from locust import HttpLocust, TaskSet
+from locust import HttpLocust, TaskSet, task
+from bs4 import BeautifulSoup
+from faker import Faker
 
-class VisitorBehavior(TaskSet):
-    def visit(self):
-        self.client.get("/")
+fake = Faker()
+
+class Helpers:
+    def parse_csrf(self, string):
+        tag = BeautifulSoup(string, "html.parser").find("input", attrs={"name":"csrfmiddlewaretoken"})
+        if tag is not None:
+            return tag["value"]
+
+class VisitorBehavior(TaskSet, Helpers):
+    @task
+    def register_user(self):
+        pass
 
 class Visitor(HttpLocust):
     task_set = VisitorBehavior
+    min_wait = 5000
+    max_wait = 10000
